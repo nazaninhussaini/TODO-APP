2a1b99ba21efb333f29c0baadc49f8c7056bd243
nazaninhussaini <banonazaninhussaini@gmail.com>
2026-01-08 21:01:56 +0430

    add code

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const addBtn = document.getElementById("addBtn");
-const todo = document.getElementById("todo").value;
-const order = document.getElementById("order").value;
+
 const saveBtn = document.getElementById("addTodo");
 const contForm = document.getElementById("con");
 const todoList =[];
+let message = "";
 
 addBtn.addEventListener("click",()=>{
     if(contForm.classList.contains("hidden")){
@@ -15,3 +15,25 @@ addBtn.addEventListener("click",()=>{
         contForm.classList.add("hidden")
     }
 })
+saveBtn.addEventListener("click",(event)=>{
+    event.preventDefault();
+    saveTodo();
+})
+function saveTodo(){
+    const todo = document.getElementById("todo").value;
+    const order = document.getElementById("order").value;
+    if(todo!==""){
+
+
+    const saveTodo = {
+        myTodo: todo,
+        importance: order
+    }
+    todoList.push(saveTodo);
+}
+else{
+    message = "لطفا یک لیست اضافه نموده و دوباره تلاش کنید.."
+}
+
+
+}
