d39f33d1193da83e68c2286688195b10b59216ee
nazaninhussaini <banonazaninhussaini@gmail.com>
2026-01-10 21:32:12 +0430

    add codes

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const addBtn = document.getElementById("addBtn");
-
+const error = document.getElementById("error")
 const saveBtn = document.getElementById("addTodo");
 const contForm = document.getElementById("con");
+const todoCont = document.getElementById("todoContanier")
 const todoList =[];
 let message = "";
 
@@ -18,8 +19,18 @@ addBtn.addEventListener("click",()=>{
 saveBtn.addEventListener("click",(event)=>{
     event.preventDefault();
     saveTodo();
+    todoList.map(todo =>{
+        showTodo(todo);
+    })
+    
+    if(message.length>0){
+        const h1 = document.createElement("h1");
+        h1.textContent = message;
+        error.appendChild(h1);
+    }
 })
 function saveTodo(){
+    error.innerHTML = "";
     const todo = document.getElementById("todo").value;
     const order = document.getElementById("order").value;
     if(todo!==""){
@@ -27,13 +38,33 @@ function saveTodo(){
 
     const saveTodo = {
         myTodo: todo,
-        importance: order
+        importance: order,
+        completed :false
     }
     todoList.push(saveTodo);
+    message = "";
 }
 else{
     message = "لطفا یک لیست اضافه نموده و دوباره تلاش کنید.."
 }
 
 
+}
+function showTodo(todo){
+    const div = document.createElement("div");
+    contForm.appendChild(div);
+    div.classList.add("todoCont");
+    const h1 = document.createElement("h1");
+    h1.textContent = todo.myTodo;
+    const circle = document.createElement("div");
+    div.append(circle,h1);
+    if(todo.importance === "important"){
+        circle.classList.add("im")
+    }
+    else if(todo.importance === "optional"){
+        circle.classList.add("optional")
+    }
+    else{
+        circle.classList.add("normal")
+    }
 }
